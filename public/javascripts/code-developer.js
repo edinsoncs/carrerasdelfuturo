526354a3d21ba833094be80a0334cb6fccdf87be
Edinson <jean_clude_dortosms@hotmail.com>
2016-09-09 12:38:02 -0300

    two

--- public/javascripts/code-developer.js
+++ public/javascripts/code-developer.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	var n = $(".jsClick");
+	var videoPlay = $(".header__Cours--Articl");
 	function webview(t) {
 		$(t).click(function(){
 			$(".none").slideToggle();
@@ -7,5 +8,23 @@ $(document).ready(function(){
 		});
 	}
 	webview(n)
+
+	function showPlay(element) {
+		$(element).click(function() {
+			$(".Articl--Figure img").fadeOut('fast');
+			$("video").css('display','block');
+
+
+			//None Cap
+			$(".container--Icon").css('display','none');
+			//Playing Video
+			document.getElementById("showvideo").play();
+
+		});
+	}
+
+	showPlay(videoPlay)
+
+
 
 });
